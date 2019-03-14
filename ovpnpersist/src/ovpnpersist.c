b553912d2449e3bcfbdcb6c14e0a5832dd210383
mark <mark@mark@kyin.org>
2019-03-14 13:20:34 -0400

    Mips Cross Compile now working "Yeah !"

--- ovpnpersist/src/ovpnpersist.c
+++ ovpnpersist/src/ovpnpersist.c
@@ -2,7 +2,7 @@
  ============================================================================
  Name        : ovpnpersist.c
  Author      : Mark Meadows
- Version     : 00.00.02
+ Version     : 00.00.03
  Copyright   : copyright Mark Meadows
  Description : ovpnpersist in C, Ansi-style
  ============================================================================
@@ -15,8 +15,10 @@ int main(void) {
 
 	printf("I am Running and Mark Is Cool");
 
-	//if ( system("ping -c 3 10.10.0.5") == 0)
-	//{
-	//	printf("Failed");
-	//}
+	if ( system("ping -c 3 10.10.0.5") == 0)
+	{
+		printf("Failed");
+	}
+
+	exit(0);
 }
