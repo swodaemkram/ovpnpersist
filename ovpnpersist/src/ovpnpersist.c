bc94f72e4f9641e1055aef81148ad19acb00250a
mark <mark@mark@kyin.org>
2019-03-14 09:59:14 -0400

    test

---
+++ ovpnpersist/src/ovpnpersist.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : ovpnpersist.c
+ Author      : Mark Meadows
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("ovpnpersist"); /* prints ovpnpersist */
+	return EXIT_SUCCESS;
+}
