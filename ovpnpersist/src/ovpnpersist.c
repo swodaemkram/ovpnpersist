7455d6649920ca85d5072ec80aaf28e1c086be53
mark <mark@mark@kyin.org>
2019-03-14 13:57:42 -0400

    This is working need to add config file

--- ovpnpersist/src/ovpnpersist.c
+++ ovpnpersist/src/ovpnpersist.c
@@ -17,7 +17,9 @@ int main(void) {
 	if ( system("ping -c 3 10.10.0.5") != 0)
 
 	{
-		printf("\nFailed\n");
+		printf("\n Connection Failed\n");
+		system ("/usr/sbin/openvpn --config /config/lt1.ovpn &");
+		system ("\n");
 	}
 
 	exit(0);
